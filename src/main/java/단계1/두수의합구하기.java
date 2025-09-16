c64237c66855686640cbbcee241fb234e3a156c4
malaheaven <yunhg95@gmail.com>
2025-09-16 10:59:09 +0900

    두수의합구하기

---
+++ src/main/java/단계1/두수의합구하기.java
@@ -0,0 +1,13 @@
+package 단계1;
+
+public class 두수의합구하기 {
+
+    public int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        두수의합구하기 solution = new 두수의합구하기();
+        System.out.println(solution.solution(2, 3));
+    }
+}
