acdeefc663aab8b731e2cde6928d9e7a32c5aa07
malaheaven <yunhg95@gmail.com>
2025-09-16 10:34:20 +0900

    두수의 차 구하기 프로그래머스

---
+++ src/main/java/단계1/두수의차구하기.java
@@ -0,0 +1,13 @@
+package 단계1;
+
+public class 두수의차구하기 {
+
+    public int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static void main(String[] args) {
+        두수의차구하기 soultion = new 두수의차구하기();
+        System.out.println(soultion.solution(2, 3));
+    }
+}
